5a9cd774f8cb72f4037ed146406a4008150d584e
Ryan Kong <rkong2@wisc.edu>
2021-09-10 20:41:02 -0500

    New GC code
    - Plot data from multiple files at once
    - Choose which samples/products you want to plot data for
    - Generate summary statistics table and output to Excel
    - New gc module to reduce clutter
    - More functionality to facilitate sanity checks

---
+++ Ryan/Repository Backup/Repository/ReadMe.txt
@@ -0,0 +1,9 @@
+*** I am interested in compiling all of this data to make a table that summarizes average titer and standard deviation. See Table S6 in attached Supplementary File. 
+
+* From March of 2021 forward, samples were analyzed for C10:1. You can delete these rows if it makes your data processing easier (ie if it is easier for each sample to have the same number of rows).
+
+* You will notice C17cp in some samples. You can ignore or delete.
+
+* The attached script that makes the bar plots is the most current. Note that as of now it is coded to expect C10:1 in each sample. The bottom block after the graph output is what arranges the titer and standard error of the mean and outputs it to Excel in the way we like to report it.
+
+* The complete list of all variants in file BTE_full_mutant_list. The number of unique data points in these spreadsheets shoudl match the number of entries in this file.
---
+++ Ryan/Repository/gcplot/.ipynb_checkpoints/gc-checkpoint.py
@@ -0,0 +1,247 @@
+import pandas as pd
+import numpy as np
+from numpy import array
+import matplotlib.pyplot as plt
+import math
+import openpyxl
+from statistics import * 
+from cycler import cycler
+from scipy import stats
+from datetime import datetime
+import os
+from openpyxl import load_workbook
+
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+    
+
+def load_data(file_list, sheet, remove_sample_list, remove_product_list, output = False):
+    # Make sure file_list is a list, even if it's a single string
+    file_list = [file_list] if isinstance(file_list, str) else file_list
+    # Create empty dataframe to hold all data
+    master_df = pd.DataFrame()
+    # Dictionary to hold miscillaenous information for review if desired
+    df_dict = {}
+    # List to hold all sample names for review if desired. Also used to filter duplicate samples across multiple spreadsheets
+    master_sample_list = []
+    
+    for file in file_list:
+        # Get the corrected concentration data
+        df, sample_list = getCorrectedConcentration(file, sheet)
+        # Remove any samples that are in remove_sample_list
+        df_removed = df[~df['Sample Group'].isin(remove_sample_list)]
+        # Remove any samples that have already been added from other sheets
+        df_removed_dup = df_removed[~df_removed['Sample Group'].isin(master_sample_list)]
+        # Identify which were removed due to remove_sample_list
+        removed = list(set(df['Sample Group']) & set(remove_sample_list))
+        # Identify which were removed due to duplicates
+        duplicates = list(set(df_removed['Sample Group']) & set(master_sample_list))
+        # Identify which were kept after removal from remove_sample_list (not duplicates)
+        sample_list_kept = list(df_removed['Sample Group'].unique())
+        
+        # Update dataframe dictionary
+        df_dict[file] = df
+        # Update master dataframe
+        master_df = master_df.append(df_removed).reset_index(drop = True)
+        
+        # Remove products that are in remove_product_list
+        master_df = master_df[~master_df['Products'].isin(remove_product_list)]
+        
+        
+        # Print if output is True
+        if output:
+            color_product_list = [red(product) if product in remove_product_list else product for product in df['Products'].unique()]
+            color_sample_list = []
+            for sample in list(df['Sample Group'].unique()):
+                if sample in remove_sample_list:
+                    color_sample_list.append(red(sample))
+                    continue
+                if sample in master_sample_list:
+                    color_sample_list.append(yellow(sample))
+                    continue
+                color_sample_list.append(green(sample))
+                
+            print('\033[1m' + file + '\033[0m')
+            print('Detected products:      [', end = '')
+            for i in range(len(color_product_list) - 1):
+                print('\'' + color_product_list[i] + '\'', end = ', ')
+            print('\'' + color_product_list[-1] + '\'', end = ']\n')
+            print('Detected sample groups: [', end = '') 
+            for i in range(len(color_sample_list) - 1):
+                print('\'' + color_sample_list[i] + '\'', end = ', ')
+            print('\'' + color_sample_list[-1] + '\'', end = ']\n')
+                
+        # Update master sample list
+        master_sample_list.extend(sample_list_kept)
+    return master_df, master_sample_list, df_dict
+
+
+def red(text):
+    return bcolors.FAIL + text + bcolors.RESET 
+
+def yellow(text):
+    return bcolors.WARNING + text + bcolors.RESET
+
+def green(text):
+    return bcolors.OK + text + bcolors.RESET
+
+
+def getCorrectedConcentration(file, sheet):
+    """
+    Filters the data in the <sheet> worksheet in the <file> excel file. Use for 'Corrected Concentration' worksheet.
+    Returns dataframe with filtered Corrected Concentration data and additional columns for grouping data by sample and product.
+    """
+    # Read excel file
+    wb = openpyxl.load_workbook(file)
+    df = pd.read_excel(file, sheetname=sheet)
+    
+    # Get sample names from first column by filtering for strings only and not counting rows with 'Peak#'. Rename to 'Sample Names'
+    sample_names = df[pd.to_numeric(df[0], errors='coerce').isnull()][0][df[0] != 'Peak#'].dropna().to_frame().rename(columns={0:'Sample Names'})
+    # Add sample names column to the original dataframe and rename column 5 (which contains the product IDs) to 'Products'
+    df = pd.concat([df,sample_names],axis=1).rename(columns={5:'Products'})
+    # Replace '_' character with ':' for propper naming convention
+    df['Products'] = df['Products'].apply(lambda product : product.replace('_',':'))
+    # Forward fill the sample names
+    df['Sample Names'] = df['Sample Names'].fillna(method = 'ffill')
+    # Get rid of number identifier (expected format is <sample name>-<sample number>). Removes the last item separated by a hyphen
+    df['Sample Group'] = df['Sample Names'].apply(lambda sample_name: "-".join(sample_name.split('-')[:-1]))
+    # Create column to identify which file the data came from
+    df['Source File'] = file
+    # Get rid of blanks in 'Corrected Concentration'
+    df = df.dropna(subset=['Corrected Concentration'])
+    return df, list(df['Sample Group'].unique())
+
+
+def dataParse(df, sample_order = None, product_order = None):
+    """
+    Uses cleaned Corrected Concentration dataframe to calculate triplicate averages (scaled and absolute) and standard errors
+    Returns two dataframes (average and standard error) with rows corresponding to samples and columns corresponding to products
+    """
+    # Calculate means, scaled means, and standard errors using pivot table-like operation
+    df_mean = pd.pivot_table(df, values = 'Corrected Concentration', index = ['Sample Group'], columns = ['Products'])
+    df_sum = list(df_mean.sum(axis = 1))
+    df_scaled = df_mean.divide(df_sum, axis='index')
+    df_err = pd.pivot_table(df, values = 'Corrected Concentration', index = ['Sample Group'], columns = ['Products'], aggfunc = stats.sem)
+    
+    # If sample order is provided, then change order
+    if sample_order != None:
+        sample_order = [sample for sample in sample_order if sample in list(df['Sample Group'])]
+        df_mean = df_mean.reindex(index = sample_order)
+        df_scaled = df_scaled.reindex(index = sample_order)
+        df_err = df_err.reindex(index = sample_order)
+    # If product order is provided, then change order
+    if product_order != None:
+        product_order = [product for product in product_order if product in list(df['Products'])]
+        df_mean = df_mean[product_order]
+        df_scaled = df_scaled[product_order]
+        df_err = df_err[product_order]
+        
+    return df_mean, df_scaled, df_err
+
+
+def summary_stats(df, sample_order = None, product_order = None):
+    """
+    Generates summary statistics of product distributions for each sample given dataframe of clean Corrected Concentrations
+    Returns dataframe containing percentage of total titer of each product for each sample group, and a final column containing total titer averages
+    """
+    df = df.copy(True)
+    # Calculate the total titer for each sample
+    tot_titer = df.groupby('Sample Names').sum()
+    # Label the sample group for each sample
+    tot_titer['Sample Group'] = tot_titer.index.to_series().apply(lambda sample_name: "-".join(sample_name.split('-')[:-1]))
+    # Create new column in original dataframe containing the total titer for each data point
+    df['Total Titer'] = df['Sample Names'].apply(lambda sample_name: tot_titer.loc[sample_name]['Corrected Concentration'])
+    # Create new column to calculate the percentage that each product contributes to the total titer
+    df['Percent of Total Titer'] = df['Corrected Concentration']/df['Total Titer']*100
+    
+    # Calculate means and standard errors using pivot table-like operation
+    df_pct = pd.pivot_table(df, values = 'Percent of Total Titer', index = ['Sample Group'], columns = ['Products'])
+    df_pct_err = pd.pivot_table(df, values = 'Percent of Total Titer', index = ['Sample Group'], columns = ['Products'], aggfunc = stats.sem)
+    
+    # Change data-type to string to allow string combination of '±' sign
+    sample_mean = df_pct.applymap(('{:,.' + str(3) + 'g}').format).astype(str)
+    sample_err = df_pct_err.applymap(('{:,.' + str(3) + 'g}').format).astype(str)
+    sample_stats = sample_mean + '±' + sample_err
+
+    # Calculate mean and standard error of total titer for each sample group and change data-type to string to allow string combination of '±' sign
+    total_mean = tot_titer.groupby('Sample Group').first()['Corrected Concentration'].map(('{:,.' + str(3) + 'g}').format).astype(str)
+    total_err = tot_titer.groupby('Sample Group').sem()['Corrected Concentration'].map(('{:,.' + str(3) + 'g}').format).astype(str)
+    total_stats = (total_mean + '±' + total_err).to_frame().rename(columns = {'Corrected Concentration':'mg/L'})
+    
+    # Get dataframe of just source files
+    source_files = df.drop_duplicates(subset = 'Sample Group').set_index('Sample Group')['Source File']
+    
+    # Combine total titer data with sample data and return summary_stats dataframe
+    summary_stats = pd.concat([sample_stats, total_stats, source_files], axis = 1)
+    
+    # If sample order is provided, then change order
+    if sample_order != None:
+        sample_order_filt = [sample for sample in sample_order if sample in sample_stats.index]
+        summary_stats = summary_stats.reindex(index = sample_order_filt)
+    # If product order is provided, then change order
+    if product_order != None:
+        product_order_filt = [product for product in product_order if product in sample_stats] + ['Source File']
+        summary_stats = summary_stats[product_order_filt]
+    
+    # Update column names with '%' symbol
+    summary_stats.columns = ['% ' + str(product) if product != 'Source File' else str(product) for product in summary_stats.columns]
+    
+    return summary_stats
+
+def set_hatch(ax, chains):
+    bars = ax.patches
+    # List of hatch patterns for each bar
+    patterns = (['']*len(ax.get_xticks()) + ['\\']*len(ax.get_xticks()))*len(chains)
+    # Set the hatch patterns
+    for bar, pattern in zip(bars, patterns):
+        bar.set_hatch(pattern)
+    return patterns
+
+
+def save_df_to_excel(df, filename, sheet_name, replace = False):
+    file_path = os.path.join(os.getcwd(), filename)
+    print(file_path)
+    file_exists = os.path.exists(file_path)
+    
+    if not file_exists:
+        df.to_excel(filename, sheet_name = sheet_name)            
+        print('Done!')
+    else:
+        # Generate workbook
+        wb = load_workbook(file_path)
+        # Generate writer engine
+        writer = pd.ExcelWriter(file_path, engine = 'openpyxl')
+        # Assigning the workbook to the writer engine
+        writer.book = wb
+        
+        if sheet_name in wb.sheetnames:
+            print('The sheet name, ' + sheet_name + ', already exists in ' + filename + '.')
+            proceed = input('Existing data in ' + sheet_name + ' will be replaced with new data. Proceed? Yes/No: ')
+            if proceed.lower() in ['yes','ye','y']:
+                df.to_excel(writer, sheet_name = sheet_name)
+                print('Done!')
+            else:
+                print('Operation cancelled.')
+        else:
+            df.to_excel(writer, sheet_name = sheet_name)
+            print('New sheet, ' + sheet_name + ', added to ' + filename + '\nDone!')
+        writer.save()
+        writer.close()
+        
+def old():
+    if file_exists and not replace:
+        print(os.path.join(os.getcwd(), filename), 'already exists.\nChange filename or set replace to \033[1mTrue\033[1m.')
+        return None
+    elif file_exists and replace:
+        proceed = input('Exisitng file will be replaced with new file. Proceed? Yes/No: ')
+        if proceed.lower() in ['yes','ye','y']:
+            df.to_excel(filename, sheet_name = sheet_name)
+            print('Existing file replaced with new.\nDone!')
+        else:
+            print('Operation cancelled.')
+    else:
+        df.to_excel(filename, sheet_name = sheet_name)            
+        print('Done!')
